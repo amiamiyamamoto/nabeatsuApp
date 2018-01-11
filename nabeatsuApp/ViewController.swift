ec00455c249692ca237059d14cec2ca42dd9778e
AMI <a_yamamoto@innovation.co.jp>
2018-01-11 12:13:36 +0900

    Initial Commit

---
+++ nabeatsuApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nabeatsuApp
+//
+//  Created by s247 on 2018/01/11.
+//  Copyright © 2018年 ami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
