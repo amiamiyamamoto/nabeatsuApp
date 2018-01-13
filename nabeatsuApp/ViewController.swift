041424a5420c9936ade640fdf55ef5726e6c1576
AMI <a_yamamoto@innovation.co.jp>
2018-01-13 14:58:06 +0900

    ボタンを押したらcountアップする

--- nabeatsuApp/ViewController.swift
+++ nabeatsuApp/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //ボタンのタップ数を数える変数
+    var count:Int = 0
+    
+    //カウントアップするラベル
+    @IBOutlet weak var countLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //ボタンを押したときの処理
+    @IBAction func countUp(_ sender: Any) {
+        count += 1
+        countLabel.text = "count\(count)"
+    }
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
