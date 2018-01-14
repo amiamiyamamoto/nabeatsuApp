31913fa20831904aaaa3197fbdd0f59786373c16
AMI <a_yamamoto@innovation.co.jp>
2018-01-14 13:44:36 +0900

    3の倍数と3が付く数字の時にあほ、5のときにナルシストの画像が表示される処理を追加

--- nabeatsuApp/ViewController.swift
+++ nabeatsuApp/ViewController.swift
@@ -23,20 +23,36 @@ class ViewController: UIViewController {
     var nabeatsu1:UIImage! = UIImage(named:"1")
     var nabeatsu2:UIImage! = UIImage(named:"2")
     var nabeatsu3:UIImage! = UIImage(named:"3")
+    var nabeatsu5:UIImage! = UIImage(named:"5")
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //画像を表示させる
-        imageView.image = nabeatsu1
+        imageView.image = nabeatsu2
     }
 
     //ボタンを押したときの処理
     @IBAction func countUp(_ sender: Any) {
+        
+        var flag3:Bool = false
+        var flag5:Bool = false
+        
         count += 1
         countLabel.text = "count\(count)"
-        
-        if count % 3 == 0 {
+
+        //3の倍数　3の付く数字かどうかの判定
+        if count % 3 == 0 || "\(count)".contains("3") {
+            flag3 = true
+        }
+        if count % 5 == 0 || "\(count)".contains("5") {
+            flag5 = true
+        }
+
+        if flag3 {
             imageView.image = nabeatsu3
+        } else if flag5 {
+            imageView.image = nabeatsu5
         } else if count % 3 == 1 {
             imageView.image = nabeatsu1
         } else {
